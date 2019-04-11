2f469b1ed322dc0fc25f8e0880021654c7458c61
Scott Byrne <scott@wsadvantage.com>
2019-04-11 16:17:23 -0300

    Improving feature generation

--- source/scripts/painting.js
+++ source/scripts/painting.js
@@ -258,9 +258,10 @@ class Painting extends Canvas {
     }
     feature.x1 = super.randBias(minUnit,cW - minUnit,bias,1,'easeOutQuad');
     feature.y1 = cH * (1 - this.horizon);
-    feature.x2 = super.randBias(-cW*1.5,cW*2.5,cW/2,1,'easeOutQuad');
+    feature.x2bias = (feature.x1 < cW / 2) cW * .33 ? : cW * .66;
+    feature.x2 = super.randBias(-cW*1.5,cW*2.5,feature.x2bias,1,'easeOutQuad');
     feature.y2 = cH;
-    feature.width = super.randBias(minUnit,cW,cW/1.6,1,'easeInQuad');
+    feature.width = super.randBias(cw / 10,cW,cW/1.6,1,'easeOutQuad');
     return feature;
   }
 
