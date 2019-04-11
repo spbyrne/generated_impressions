a01c9ba060849f0898f9137c53cdea25f7ae7f6a
Scott Byrne <scott@wsadvantage.com>
2019-04-11 16:58:14 -0300

    Testing multi painting generation and display

--- source/scripts/generated_impressions.js
+++ source/scripts/generated_impressions.js
@@ -2,4 +2,4 @@ const Artist = require('./artist.js');
 
 let artist = new Artist('Anna Denson');
 let container = document.querySelector('.wrapper');
-artist.paint().display(container);
+artist.paint().paint().paint().paint().paint().paint().paint().paint().paint().display(container);
