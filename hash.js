16adc612e50b5e92ad111a5ab696ab1d929ec365
Scott Byrne <scott@wsadvantage.com>
2019-04-16 12:35:28 -0300

    Fixing node hash script

--- hash.js
+++ hash.js
@@ -2,6 +2,9 @@ var exec = require('child_process').exec;
 var seedSetter = callHashFile();
 var fs = require('fs');
 
+const indexPath = './public/index.html'
+const fd = fs.openSync(indexPath, 'w')
+
 function setSeed(hash) {
   let seed = hash.slice(0, 7);
   fs.readFile('./source/index.html', 'utf8', function (err,data) {
@@ -10,7 +13,7 @@ function setSeed(hash) {
     }
     var result = data.replace(/{{ hash }}/g, seed);
   
-    fs.writeFile('./public/index.html', result, 'utf8', function (err) {
+    fs.writeFile(indexPath, result, 'utf8', function (err) {
        if (err) return console.log(err);
     });
   });
